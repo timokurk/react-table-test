f7dbac036fc1c4820d3bf036a982be675120be70
timokurk <35221753+timokurk@users.noreply.github.com>
2022-08-31 10:32:50 +0300

    fix timer

--- src/components/TableHeader/tableheader.jsx
+++ src/components/TableHeader/tableheader.jsx
@@ -4,8 +4,11 @@ const TableHeader = ({ title }) => {
   const [timer, setTimer] = useState(0);
 
   useEffect(() => {
-    setTimer(previousTimer => previousTimer + 1);
-  }, [timer]);
+    const intervalId = setInterval(() => {
+      setTimer(prevTimer => prevTimer + 1); // toinen ratkasu laittaa timer + 1 ja useEffectin dependancyyn timer
+    }, 1000)
+    return () => clearInterval(intervalId); // välttää memory leakin (sekunnitkin pomppi vähän GUI:ssa)
+  }, []);
 
   return (
     <thead>
